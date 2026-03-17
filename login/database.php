30b5848610d428786d0e8870e5cdfdb19818cbd4
1509aaryaa@gmail.com <1509aaryaa@gmail.com>
2026-03-17 18:56:32 +0530

    database updated

--- login/database.php
+++ login/database.php
@@ -1,15 +1,17 @@
 <?php
 
-$host = "localhost";
-$dbname = "login_db";
-$username = "root";
-$password = "";
+$host = "dypnotes-db.cviuomgcokjp.ap-south-1.rds.amazonaws.com";
+$dbname = "dypnotes";   // ✅ FIXED
+$username = "admin";    // ✅ FROM AWS
+$password = "b69ZLZX4ZwPuyTV";
 
-$mysqli = new mysqli(hostname: $host,
-                     username: $username,
-                     password: $password,
-                     database: $dbname);
-                     
+$mysqli = new mysqli(
+    $host,
+    $username,
+    $password,
+    $dbname
+);
+
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
 }
